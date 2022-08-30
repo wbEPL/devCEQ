461c50709fec89a3ee548ecbb5252227e911e824
Eduard Bukin <edwardbukin@gmail.com>
2022-08-30 20:31:33 +0200

    Adding continuous renderring workflow.

---
+++ dev/16-inputs-ui-dev.R
@@ -0,0 +1,60 @@
+golem::detach_all_attached()
+# golem::document_and_reload()
+
+library(shiny)
+library(tidyverse)
+
+pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+
+path <- "../data-raw/complex-inputs-structure.xlsx"
+inp_raw_str <- path %>% load_input_xlsx()
+inp_tab_str <- path %>% load_inputtabs_xlsx()
+inp_table_str <- path %>% load_inputtables_xlsx()
+
+# === === === === === === === === === === === === === === === === === === ===
+# Function that we develop:   === === === === === === === === === === === ===
+# === === === === === === === === === === === === === === === === === === ===
+
+
+# === === === === === === === === === === === === === === === === === === ===
+# Manual testing if it works  === === === === === === === === === === === ===
+# === === === === === === === === === === === === === === === === === === ===
+
+local_inp_str_fn <- gen_inp_str_front(inp_table_str)
+local_tab_ui_fn <- gen_tabinp_ui_front(inp_tab_str, inp_table_str)
+
+all_outs <-
+  local_inp_str_fn(inp_raw_str, n_choices = 5, ns = NS(NULL)) %>%
+  local_tab_ui_fn()
+
+# gen_inp_str(inp_raw_str, n_choices = 1, ns = NS(NULL))
+# test_genui_fn(inp_raw_str,
+#               gen_inp_str = gen_inp_str,
+#               gen_ui_fn = local_tab_ui_fn,
+#               full = T)
+
+
+# === === === === === === === === === === === === === === === === === === ===
+# Running reloading the app:  === === === === === === === === === === === ===
+# === === === === === === === === === === === === === === === === === === ===
+
+server <- function(input, output, session) {
+  mod_dyn_inp_srv(
+    NULL,
+    inp_raw_str = inp_raw_str,
+    n_choices = reactive(2),
+    n_max_choices = reactive(3),
+    upd_inp = reactive(NULL),
+    reseter = reactive(NULL),
+    inp_str_fn = gen_inp_str,
+    ui_gen_fn = local_tab_ui_fn,
+    gen_header_ui = gen_header_ui
+  )
+}
+
+
+
+fluidPage(column(2, wellPanel(mod_inp_switches_ui(NULL))),
+          column(10, mod_dyn_inp_ui(NULL))) %>%
+  shinyApp(., server)
+
---
+++ dev/17-inputs-ui-run-dev.R
@@ -0,0 +1,3 @@
+
+options(shiny.autoreload = TRUE)
+shiny::runApp(appDir = "dev/16-inputs-ui-dev.R")

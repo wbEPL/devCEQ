0c617e712f75da5578af887706335bc462ac4589
Eduard Bukin <edwardbukin@gmail.com>
2025-10-24 08:07:56 -0400

    Generating microdata for testing

---
+++ R/fct_variables.R
@@ -0,0 +1,158 @@
+#' Key variables names and labels
+#'
+#' @returns data frame with variables names
+#' @importFrom shiny isTruthy
+#' @importFrom stringr str_c
+#' @importFrom dplyr mutate select filter
+#'
+#' @export
+get_var_nm <- function(vars = NULL, suffix = "_pc") {
+  
+  var_dic_local <- f_var_dic_default()
+
+  if (exists("f_var_dic", mode = "function")) {
+    var_dic_local <- f_var_dic()
+  }
+
+  dta <-
+    var_dic_local |>
+    mutate(var_title = var_title |> fct_keep_dup_string() |> as_factor()) |>
+    select(var, var_title, factor)
+
+  if (!missing(suffix) && !is.null(suffix)) {
+    dta <- dta %>% mutate(var = str_c(var, suffix))
+  }
+
+  if (!missing(vars) && !is.null(vars)) {
+    dta <-
+      dta %>%
+      filter(var %in% vars) %>%
+      mutate(
+        var_title = factor(var_title, levels = var_title, labels = var_title)
+      )
+  }
+
+  return(dta)
+}
+
+
+#' @describeIn get_var_nm Income Concepts variables IDs and labels returned in a data frame
+#'
+#' @returns a data frame with variables names
+#'
+#' @export
+get_inc_nm <- function(suffix = "_pc") {
+  inc_vars <- f_var_inc_default()
+  if (exists("f_var_inc", mode = "function")) {
+    inc_vars <- f_var_inc()
+  } 
+  inc_vars |>
+    str_c(., suffix) %>%
+    get_var_nm(suffix = suffix)
+}
+
+#' @describeIn get_var_nm Weight variable IDs and labels in a data frame
+#'
+#' @returns a data frame with variables names
+#'
+#' @export
+get_wt_nm <- function(suffix = NULL) {
+  wt_var <- f_var_wt_default()
+  if (exists("f_var_wt", mode = "function")) {
+    wt_var <- f_var_wt()
+  } 
+
+  wt_var %>%
+    str_c(., suffix) %>%
+    get_var_nm(suffix = suffix) %>%
+    pull(var)
+}
+
+#' @describeIn get_var_nm Helper to label duplicated variable names as different factors levels.
+#' 
+#' @export
+fct_keep_dup_string <- function(val) {
+  zero_width_space <- "\u200b"
+  val_chr <- as.character(val)
+  n_dup <- ave(seq_along(val_chr), val_chr, FUN = seq_along)
+  val_unique <- paste0(val_chr, strrep(zero_width_space, n_dup - 1))
+  val_unique
+}
+
+
+#' @describeIn get_var_nm Variables dictionary 
+#'
+f_var_dic_default <- function() {
+    tribble(
+      ~factor,  ~ var                , ~var_title,
+
+      # Benefits
+      1, "ben_pension"   , "Pension benefits",
+      1, "ben_unemployment", "Unemployment benefits",
+      1, "ben_total"     , "Benefits total",
+
+      # Direct Transfers
+      1, "dtr_prog1"  , "Directs transfer progarm 1",
+      1, "dtr_prog2"  , "Directs transfer progarm 2",
+      1, "dtr_total"  , "Direct transfers total",
+
+
+      # Subsidy
+      1, "sub_energy"      , "Energy subsidy",
+      1, "sub_food"      , "Food subsidy",
+      1, "sub_total"      , "Subsidies total",
+  
+      # Direct taxes
+      -1, "dtx_prog1"      , "Direct taxes program 1",
+      -1, "dtx_prog2"      , "Direct taxes program 2",
+      -1, "dtx_prog3"      , "Direct taxes on self-employed",
+      -1, "dtx_total"      , "Direct taxes on employers income",
+
+      # Indirect taxes
+      -1, "itx_vat"           , "Indirect taxes: VAT",
+      -1, "itx_excise"        , "Indirect taxes: Excise duties",
+      -1, "itx_total"         , "Indirect taxes, total",
+
+      # # Contributions for health and pension
+      # -1, "dtp_pen", "Pension contribution paid by employees",
+      # -1, "dtp_health", "Health contribution paid by employees",
+      # -1, "dtp_total", "Total contributions ",
+
+      
+      # In kind
+      1, "ink_helth"    , "Health",
+      1, "ink_education", "Education",
+      1, "ink_total"    , "In-kind transfers total",
+
+      # Income concepts
+      1,        "ym"             ,  "Market income",
+      1,        "yp"             ,  "Market income plus pension",
+      1,        "yn"             ,  "Net market income" ,
+      1,        "yg"             ,  "Gross income" ,
+      1,        "yd"             ,  "Disposable income" ,
+      1,        "yc"             ,  "Consumable income" ,
+      1,        "yf"             ,  "Final income" ,
+
+      # Auxiliary variables
+      1,        "hhwt"             ,  "Weight: households" ,
+      1,        "hhid"             ,  "HH id" ,
+      1,        "hhsize"           ,  "HH size",
+
+      1,       "group_1"         ,  "Gender typology",
+      1,       "group_2"         ,  "Urban/Rural typology",
+      1,       "group_3"         ,  "Age group typology"
+
+    )
+}
+
+#' @describeIn get_var_nm Weight variables names 
+#'
+f_var_wt_default <- function() {
+  "hhwt"
+}
+
+#' @describeIn get_var_nm Income variables names
+#'
+f_var_inc_default <- function() {
+    c("ym", "yp", "yg", "yd", "yc", "yf")
+}
---
+++ data-raw/microdata.R
@@ -0,0 +1,175 @@
+## code to prepare `DATASET` dataset goes here
+
+
+pkgload::load_all()
+
+# f_var_dic_default()
+
+# Simulate a microdata dataset with varaibles that correspond to the variable dictionary
+
+n <- 1000
+n_hh <-n
+hh_size <- sample(1:6, n_hh, replace = TRUE, prob = c(0.2, 0.3, 0.25, 0.15, 0.07, 0.03))
+
+# Gender typology
+group_1 <- sample(c("Male", "Female"), n_hh, replace = TRUE, prob = c(0.62, 0.38))
+group_2 <- sample(c("Urban", "Rural"), n_hh, replace = TRUE, prob = c(0.3, 0.7))
+group_3 <- sample(c("Agriculture", "Services", "Manufacturing"), n_hh, replace = TRUE, prob = c(0.5, 0.2, 0.3))
+
+set.seed(123)
+
+# Market inceomd --------------------------------------------------------
+ym <- rlnorm(n, meanlog = 3, sdlog = .5) * hh_size / (runif(n, 0.8, 1.2) * median (hh_size))
+ym[sample(1:n, n * 0.05)] <- 0
+ym[sample(1:n, n * 0.01)] <- max(ym) * 5
+
+
+## Simlate benefits --------------------------------------------------------
+
+sim_ben_share_value <- function(n, base_value, share_ben = 0.3, min_share = 0.1, max_share = 0.5,  if_zero = rep(FALSE, n)) {
+  ben_values <- rep(0, n)
+  ben_idx <- sample(1:n, n * share_ben)
+  ben_values[ben_idx] <- base_value[ben_idx] * runif(length(ben_idx), min_share, max_share)
+  ben_values[if_zero] <- 0
+  return(ben_values)
+}
+
+# Pension benefits:
+ben_pen <- sim_ben_share_value(n, ym, 0.3, 0.6, 1.7)
+ben_unemp <- sim_ben_share_value(n, ym, 0.2, 0.3, 0.8, ben_pen == 0)
+
+# Market income plys pension benefits ---------------------------------------------
+yp = ym + ben_pen
+
+# Direct taxes ----------------------------------------------------------------
+
+pay_t1 <- rep(0, n) # Most pay direct taxes
+pay_t1_cond <- ben_unemp == 0 & ym > quantile(ym, 0.15)
+pay_t1[pay_t1_cond] <- ym[pay_t1_cond] * 0.15
+
+pay_t2 <- rep(0, n) # Employees
+pay_t2_cond <- ben_unemp == 0 & ben_pen == 0 & ym > quantile(ym, 0.45)
+pay_t2[pay_t2_cond] <- ym[pay_t2_cond] * 0.10
+
+pay_t3 <- rep(0, n) # Unemployed
+pay_t3_cond <- ben_unemp == 0 & ben_pen == 0 & pay_t2_cond == 0 & pay_t1_cond == 0
+pay_t3[pay_t3_cond] <- ym[pay_t3_cond] * 0.10
+
+dtx_prog1 <- pay_t1
+dtx_prog2 <- pay_t2
+dtx_prog3 <- pay_t3
+
+dtx_total <- dtx_prog1 + dtx_prog2 + dtx_prog3
+
+# Net market income ---------------------------------------------------------
+
+yn <- ym - dtx_prog1 - dtx_prog2 - dtx_prog3
+yn[yn < 0] <- 0
+
+# Direct transfers ---------------------------------------------------------
+
+dtr_prog1 <- sim_ben_share_value(n, ym, 0.5, 0.6, 1.7, yn < quantile(yn, 0.15))
+dtr_prog2 <- sim_ben_share_value(n, ym, 0.75, 0.3, 0.4, yn < quantile(yn, 0.15))
+
+# Direct subsidies ----------------------------------------------------------
+dsu_unempl <- ben_unemp
+
+# Gross income  ---------------------------------------------------------
+yg <- yp  +  dtr_prog1 + dtr_prog2 + dsu_unempl
+
+# Disposable income ---------------------------------------------------------
+yd <- yg - dtx_prog1 - dtx_prog2 - dtx_prog3
+
+# Indirect subsidies and taxes ---------------------------------------------
+sub_energy <- sim_ben_share_value(n, hh_size, 0.75, 0.5, 0.7)
+sub_food <- sim_ben_share_value(n, hh_size, 0.15, 0.5, 0.7, yd < quantile(yd, 0.25))
+sub_total <- sub_energy + sub_food
+
+# Consumption ----------------------------------------------------------
+itx_vat <- sim_ben_share_value(n, yd, 0.80, 0.07, 0.12, yd > 0)
+itx_excise <-sim_ben_share_value(n, yd, 0.70, 0.07, 0.12, yd > 0)
+itx_total <- itx_vat + itx_excise
+
+# Consumable income ---------------------------------------------------------
+yc <- yd - itx_vat - itx_excise + sub_energy + sub_food
+
+
+# In kind ---------------------------------------------------------------
+ink_helth <- sim_ben_share_value(n, hh_size, 0.90, 0.5, 0.75) * mean(yg) / 10
+ink_education <- sim_ben_share_value(n, hh_size, 0.80, 0.5, 0.8, hh_size > 2) * mean(yg) / 15
+ink_total <- ink_helth + ink_education
+
+# Final income ------------------------------------------------------------
+yf <- yc + ink_helth + ink_education
+
+# Micro dta
+dta_hh <- 
+tibble(
+  hhid = 1:n_hh,
+  hhwt = runif(n_hh, 0.5, 2),
+  hhsize = hh_size,
+  group_1 = group_1,
+  group_2 = group_2,
+  group_3 = group_3,
+  ym = ym,
+  yn = yn,
+  yp = yp,
+  yg = yg,
+  yd = yd,
+  yc = yc,
+  yf = yf,
+  ben_pension = ben_pen,
+  ben_unemployment = ben_unemp,
+  ben_total = ben_pen + ben_unemp,
+  dtx_prog1 = dtx_prog1,
+  dtx_prog2 = dtx_prog2,
+  dtx_prog3 = dtx_prog3,
+  dtx_total = dtx_total,
+  dtr_prog1 = dtr_prog1,
+  dtr_prog2 = dtr_prog2,
+  sub_energy = sub_energy,
+  sub_food = sub_food,
+  sub_total = sub_total,
+  itx_vat = itx_vat,
+  itx_excise = itx_excise,
+  itx_total = itx_total,
+  ink_helth = ink_helth,
+  ink_education = ink_education,
+  ink_total = ink_total
+)
+
+# Sim direct taxes --------------------------------------------------------
+
+# # Add 2 types of direct taxes proportional to the market income
+
+# # Direct tax on employees income: 10% flat rate
+# dtx_inc <- market_income * 0.1
+
+# # Direct tax on employers income: apply to 50% of hh only, 5% flat rate
+# dtx_con <- rep(0, n)
+# dtx_con_idx <- sample(1:n, n * 0.5)
+# dtx_con[dtx_con_idx] <- market_income[dtx_con_idx] * 0.05
+
+
+  
+# ggplot() + 
+#   geom_density(aes(x = market_income, colour = "Market income")) + 
+#   geom_density(aes(x = pension_benefits, colour = "Pension")) +
+#   scale_x_log10()
+
+  
+# ggplot() + geom_density(aes(x = microsim_dta$ym)) + scale_x_log10()
+
+
+# Final data set
+# miro_dta <-
+dta_hh |>
+  pivot_longer(
+    cols = -c(hhid, hhwt, hhsize, group_1, group_2, group_3),
+    names_to = "var",
+    values_to = "value"
+  ) |>
+  ggplot() +
+  geom_density(aes(x = value, colour = var)) +
+  scale_x_log10()
+

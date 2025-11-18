efe1210fe1a944aa285cf5b1a6e0338646027117
Eduard Bukin <edwardbukin@gmail.com>
2025-11-18 12:54:47 -0500

    Diagnostic of gg plots

--- R/m_diagnostics.R
+++ R/m_diagnostics.R
@@ -48,13 +48,41 @@ m_diagnostics_srv <-
 
         output$diag_text <- renderPrint({
           if (in_devmode()) {
-            str(out())
+            out <- out()
+            out$ggs <- names(out$ggs)
+            str(out, max.level = 3)
           } 
         })
       }
     )
   }
 
+
+
+
+#' @describeIn m_diagnostics Test app for m_diagnostics module
+#' @export
+test_m_diagnostics <- function() {
+  library(shiny)
+
+  ui <- page_fluid(
+    m_diagnostics_ui("diag1")
+  )
+
+  server <- function(input, output, session) {
+    test_obj <- reactive({
+      list(
+        a = rnorm(100),
+        b = data.frame(x = rnorm(50), y = runif(50)),
+        c = list(sub1 = letters[1:10], sub2 = matrix(1:9, nrow = 3))
+      )
+    })
+
+    m_diagnostics_srv("diag1", out = test_obj)
+  }
+
+  shinyApp(ui, server)
+}
 
 
 

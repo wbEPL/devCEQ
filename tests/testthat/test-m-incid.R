90f667b77c937861ab590f4609309f11a4ac19b5
Eduard Bukin <edwardbukin@gmail.com>
2025-12-16 17:19:24 -0500

    Expanding incidences tests

--- tests/testthat/test-m-incid.R
+++ tests/testthat/test-m-incid.R
@@ -65,16 +65,16 @@ dta_deciles |>
   f_agg_by_decile(
     var_decile = c("ym___decile", "yn___decile", "yp___decile"),
     var_agg = c("ym", "yn", "yp",  "dtx_prog1", "dtx_prog2"),
-    var_group = "group_2",
+    var_group = c("all", "group_2", "group_1"),
     wt_var = get_wt_nm()
-  )
+  ) 
 
 
 dta_deciles |>
   f_agg_by_decile(
     var_decile = c("ym___decile", "yn___decile", "yp___decile"),
     var_agg = c("ym", "yn", "yp",  "dtx_prog1", "dtx_prog2"),
-    var_group = "group_2",
+    var_group = "all",
     wt_var = get_wt_nm()
   )
 
@@ -82,94 +82,92 @@ dta_deciles |>
 dec_agg <- 
   dta_sim_local |>
   f_calc_deciles_by_sim(
-    dec_var = get_inc_nm()$var,
+    dec_var = get_inc_nm()$var[1:2],
     wt_var = get_wt_nm(),
     n_dec = 3
   ) |>
   f_agg_by_decile_by_sim(
-    var_decile = str_c(get_inc_nm()$var, "___decile"),
-    var_agg = c(get_inc_nm()$var, "dtx_prog1", "dtx_prog2"),
-    wt_var = get_wt_nm(),
-    n_dec = 10
+    var_decile = str_c(get_inc_nm()$var[1:2], "___decile"),
+    var_agg = c(get_inc_nm()$var[2:3], "dtx_prog1"),
+    var_group = c("all", "group_1"), 
+    wt_var = get_wt_nm()
   )
 
-
-dec_agg |> f_prep_incidence()
-
+dec_agg |> count(decile_var, var)
 
 
-# dec_agg2 <-
-dec_agg |>
-  f_prep_incidence() |>
-  f_add_measure_labels() |>
-  f_add_var_labels() |>
-  f_add_var_labels(to_var = "decile_var") |>
-  group_by(measure, decile_var, var) |>
-  nest() |>
-  ungroup() |>
-  # Preserve original factor order
-  mutate(
-    across(where(is.factor), ~ as.numeric(.), .names = "order_{.col}"),
-    across(where(is.factor), as.character)
+dec_agg |> f_calc_incidence() |> count(decile_var, var, measure)
+
+# Formatting incidences data
+dec_agg |> f_calc_incidence() |> f_format_incidence() |> 
+  count(`Decile by`, Variable, Statistics)
+
+# Filtering clean incidences data
+dec_agg |> 
+  f_calc_incidence() |> 
+  f_format_incidence() |> 
+  f_filter_grouped_stats("all") |> 
+  # count(Statistics)
+  f_filter_var_generic("Relative incidence, % of Market income", "measure") |> 
+  count(Statistics)
+
+
+# Full process ------------------------------------------------------------
+
+n_dec <- 5
+agg_var <- c("ym", "dtx_prog1")
+dec_var <- c("ym", "yn") #get_inc_nm()$var
+wt_var <- get_wt_nm()
+group_var <-  get_group_nm()$var[-length(get_group_nm()$var)]
+
+
+# Step 1. Calcualte incidences
+dta_1_incid <-
+  dta_sim_local |>
+  f_calc_deciles_by_sim(
+    dec_var = dec_var,
+    wt_var = wt_var,
+    n_dec = n_dec
   ) |>
-  rowwise() |>
-  mutate(measure = glue::glue_data(cur_data(), measure)) |>
-  ungroup() |>
-  mutate(
-    across(
-      where(is.character),
-      ~ as_factor(.x) |> fct_reorder(get(paste0("order_", cur_column())))
-    )
+  f_agg_by_decile_by_sim(
+    var_decile = str_c(dec_var, "___decile"),
+    var_agg = agg_var,
+    var_group = group_var,
+    wt_var = wt_var
   ) |>
-  select(-starts_with("order_")) |>
-  unnest(cols = c(data)) |>
-  f_rename_cols()
+  f_calc_incidence() |>
+  f_format_incidence() 
 
 
+# Step 2. Filter appropriate groupping variables ------------------------------
+
+all_measures <- dta_1_incid |> select(any_of(f_get_colname("measure"))) |> pull(1) |> unique() |> as.character()
+measure_fltr <- all_measures[1]
+
+all_decile_by <- dta_1_incid |> select(any_of(f_get_colname("decile_var"))) |> pull(1) |> unique() |> as.character()
+decile_fltr <- all_decile_by[1]
+
+all_groups <- dta_1_incid |> select(any_of(f_get_colname("group_var"))) |> pull(1) |> unique() |> as.character()
+grpoup_fltr <- all_groups[2]
+
+all_vars <- dta_1_incid |> select(any_of(f_get_colname("var"))) |> pull(1) |> unique() |> as.character()
+var_fltr <- all_vars[1]
+
+dta_1_incid |> 
+  f_filter_grouped_stats(grpoup_fltr) |>
+  f_filter_var_generic(measure_fltr, "measure") |>
+  f_filter_var_generic(decile_fltr, "decile_var") |>
+  f_filter_var_generic(var_fltr, "var")  |>
+  f_plot_gg(
+    x_var = "decile",
+    y_var = "value",
+    y_lab = f_get_app_text("decile"),
+    color_var = "group_val",
+    facet_var = "sim",
+    type = "bar"
+  ) 
 
 
-
-
-# Make a function for aggregating by deciles across simulations and computing relative, absolute and level incidence
-
-
-agg_sims_by_deciles(
-  dec_by = input$by_income,
-  dec_vars = dec_vars,
-  wt_var = get_wt_nm(),
-  n_dec = n_dec_debounced(),
-  get_var_fn = get_var_nm,
-  ...
-) 
-
-## Poverty aggregation logic --------------------------------------------
-f_calc_povineq(
-  dta = dta_sim_local$policy1$policy_sim_raw,
-  var_inc = c("ym"),
-  var_wt = "hhwt",
-  pl_var = "pl_190",
-  group_var = "total"
-)
-
-# Poverty by groups --------------------------------------------
-f_calc_povineq_by(
-  dta = dta_sim_local$policy1$policy_sim_raw,
-  var_inc = c("ym", "yn", "yp", "yg", "yd", "yc", "yf"),
-  var_wt = "hhwt",
-  pl_var = "pl_nat",
-  group_vars = c("total", "group_1", "group_2")
-) |>
-  count(var)
-  count(group_var, group_val)
-
-# Poverty across all simulations --------------------------------------------
-f_calc_povineq_by_sims(
-    dta_sim = dta_sim_local,
-    var_inc = c("ym", "yn", "yp", "yg", "yd", "yc", "yf"),
-    var_wt = "hhwt",
-    pl_var = "pl_nat",
-    group_vars = c("total", "group_1")
-  )
 
 # Wrapper for all simulaiton with exaustive table of results ---------------
 dta_sim_local |> f_calc_pov_stats()
